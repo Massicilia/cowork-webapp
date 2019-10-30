6a13307cf5bded8a96e2223272d66b404ae2a27c
Massicilia <massicilia.nedir@outlook.com>
2019-10-31 00:58:01 +0100

    api-request users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
-import { UsersDetailsComponent } from './users-details/users-details.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
-
+import { UsersDetailsComponent } from './users-details/users-details.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { LoanrequestCreationComponent } from './loanrequest-creation/loanrequest-creation.component';
 const routes: Routes = [
-  { path: 'user/users', component: UsersDetailsComponent}
+  { path: '', pathMatch: 'full', redirectTo: 'user/auth'},
+  { path: 'user/auth', component: AuthentificationComponent},
+  { path: 'user/users', component: UsersDetailsComponent},
+  { path: 'user/:uuid', component: UserDetailsComponent},
+  { path: 'loanrequest', component: LoanrequestCreationComponent}
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
   ],
   exports: [ RouterModule]
 })
